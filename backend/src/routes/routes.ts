f19d872d199a18c08326c5417da4a242f7cc0ef3
izabel alcantara <izaworkingre@gmail.com>
2024-08-08 23:53:23 -0300

    feature: criando userController

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { productController } from "../controllers/product.controller"; 
+import {userController} from "../controllers/user.controllers";
 
 const router = Router();
 
@@ -9,6 +10,19 @@ router.get("/products", productController.readAll);
 router.get("/product/:id", productController.read);
 router.put("/product/:id", productController.update);
 router.delete("/product/:id", productController.delete);
+
+
+//rotas usuário
+router.post('/users', userController.create);
+router.get('/users/:id', userController.getUserById);
+router.get('/users', userController.getAllUsers);
+router.put('/user/:id', userController.updateUser);
+router.delete('/users/:id', userController.deleteUser);
+router.get('/users/:id/messages', userController.getUserMessages);
+router.get('/users/:id/orders', userController.getUserOrders);
+router.get('/users/:id/cart', userController.getUserCart);
+router.get('/users/:id/favorites', userController.getUserFavorites);
+
 
 
 export default router;
