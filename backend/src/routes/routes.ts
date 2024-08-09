15df2e45bb0d84e9d64c1ef5dcc91f034351d698
franciscoflorencio <franciscotaf@dcc.ufrj.br>
2024-08-08 23:21:24 -0300

    feature: produto controller

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,1 +1,14 @@
+import { Router } from "express";
+import { productController } from "../controllers/product.controller"; 
 
+const router = Router();
+
+//products
+router.post("/product", productController.create);
+router.get("/products", productController.readAll);
+router.get("/product/:id", productController.read);
+router.put("/product/:id", productController.update);
+router.delete("/product/:id", productController.delete);
+
+
+export default router;
