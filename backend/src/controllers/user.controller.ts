a2e97cebb36b528fbd672a5cf333bb69fc822d9d
franciscoflorencio <franciscotaf@dcc.ufrj.br>
2024-08-12 20:51:37 -0300

    feature: ultimo controller implementado, o de mensagem, junto com suas 3 rotas devidamente funcionais e testadas pelo Postman

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -44,34 +44,25 @@ class UserController {
     }
   }
 
-  public async login(request: Request, response: Response) {
-    const { email, password } = request.body;
-
+  public async login (request: Request, response: Response) {
     try {
-      const user = await prisma.user.findUnique({
-        where: { email: email }
-      });
+        const { email, password } = request.body;
+        const user = await prisma.user.findUnique({ where: { email: email } });
+        if (!user)
+            return response.status(404).json({ message: 'User not found' });
 
-      if (!user) {
-        return response.status(400).json({ message: "Usuário não encontrado!" });
-      }
-
-      const isPasswordValid = auth.checkPassword(password, user.hash, user.salt);
-      if (!isPasswordValid) {
-        return response.status(400).json({ message: "Senha incorreta!" });
-      }
-
-      const token = auth.generateJWT(user);
-
-      return response.status(200).json({ message: "Autenticação bem-sucedida", token: token });
-
+        if (auth.checkPassword(password, user.hash, user.salt))
+            return response.status(200).json({ token: auth.generateJWT(user) });
+        else 
+            return response.status(401).json({ message: 'Invalid Password' });
     } catch (error) {
-      return response.status(500).json({
-        messageError: "Erro interno no servidor",
-        error,
-      });
+        return response.status(500).json({ 
+            messageError: "Server Internal Error",
+            error: error
+        });
     }
   }
+
   public async getUserById(request: Request, response: Response) {
     try {
       const { id } = request.params;
